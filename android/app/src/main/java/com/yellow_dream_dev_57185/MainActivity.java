57d3194b7b377f80b93f9fd3a993ee09af431e61
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 08:17:30 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/yellow_dream_dev_57185/MainActivity.java
@@ -0,0 +1,15 @@
+package com.yellow_dream_dev_57185;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "yellow_dream_dev_57185";
+  }
+}
